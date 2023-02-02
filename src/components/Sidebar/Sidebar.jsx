bb598d3cfb3cd2d477cb17f292bb91ac5a5949c2
Jefferson Souza <jefferson.souza@neki-it.com.br>
2023-02-02 13:03:53 -0300

    feat(Sidebar): componentiznado sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1675329142755-2e79cb833a8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/dev-jefferson-souza.png"
+        />
+        <strong>Jefferson Souza</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
