df17152bd1b5af5b44c1b5e8f09f0deafac4391c
Jefferson Souza <jefferson.souza@neki-it.com.br>
2023-02-02 13:00:21 -0300

    feat(Header):  componente header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import IgniteLogo from "../../assets/ignite-logo.svg";
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={IgniteLogo} alt="Logotipo do Ignite" />
+    </header>
+  );
+}
