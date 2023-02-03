14d7494bc8f7caf40c3978f50c19e59f0a434b54
Jefferson Souza <jefferson.souza@neki-it.com.br>
2023-02-03 12:00:46 -0300

    feat(Avatar): componentizando avatar

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from "../Avatar/Avatar";
+import { Comment } from "../Comment/Comment";
 import styles from "./Post.module.css";
 
 export function Post(props) {
@@ -5,10 +7,7 @@ export function Post(props) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/dev-jefferson-souza.png"
-          />
+          <Avatar src="https://github.com/dev-jefferson-souza.png" />
           <div className={styles.authorInfo}>
             <strong>Jefferson Souza</strong>
             <span>Web Developer</span>
@@ -42,6 +41,12 @@ export function Post(props) {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "../Avatar/Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -10,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/dev-jefferson-souza.png"
-        />
+        <Avatar src="https://github.com/dev-jefferson-souza.png" />
         <strong>Jefferson Souza</strong>
         <span>Web Developer</span>
       </div>
