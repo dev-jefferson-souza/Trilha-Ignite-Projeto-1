d8c37908a893191e2eca0c1cef9246ad798bc9f2
Jefferson Souza <jefferson.souza@neki-it.com.br>
2023-02-04 21:34:41 -0300

    feat(Comment): função de aplaudir

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { Avatar } from "../Avatar/Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeCount() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -26,10 +33,7 @@ export function Comment({ content, onDeleteComment }) {
               </time>
             </div>
 
-            <button
-              title="Deletar comentário"
-              onClick={() => handleDeleComment()}
-            >
+            <button title="Deletar comentário" onClick={handleDeleComment}>
               <Trash size={20} />
             </button>
           </header>
@@ -37,9 +41,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button title="Aplaudir comentário">
+          <button title="Aplaudir comentário" onClick={handleLikeCount}>
             <ThumbsUp size={20} />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
